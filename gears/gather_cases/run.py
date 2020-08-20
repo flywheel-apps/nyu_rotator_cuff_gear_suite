228ca670465780c6d340837b150bb642a5386676
Joshua Jacobs <joshuajacobs@flywheel.io>
2020-08-20 19:12:31 +0000

    Remove tst

--- gears/gather_cases/run.py
+++ gears/gather_cases/run.py
@@ -83,8 +83,7 @@ def main(context):
 
 
 if __name__ == "__main__":
-    tst = "/home/joshuajacobs/Projects/2020.03.13.NYU.Tear_Assessment/Data/gather-cases-0.0.1-dev-e_5ed03221dd4e560535c2ce65"
-    with gear_toolkit_context.GearToolkitContext(tst) as gear_context:
+    with gear_toolkit_context.GearToolkitContext() as gear_context:
         gear_context.init_logging()
         exit_status = main(gear_context)
 
