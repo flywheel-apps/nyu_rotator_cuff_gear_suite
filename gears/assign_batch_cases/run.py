c2b08c263b2b7640f21d4c3aa7a6907c63362c92
Joshua Jacobs <joshuajacobs@flywheel.io>
2021-02-25 19:53:45 +0000

    More adjustments

--- gears/assign_batch_cases/run.py
+++ gears/assign_batch_cases/run.py
@@ -38,7 +38,7 @@ def main(context):
         destination_id = context.destination["id"]
         analysis = fw_client.get(destination_id)
         source_project = fw_client.get(analysis.parents["project"])
-        reader_group_id = "test_group"  # "readers"
+        reader_group_id = "readers"
 
         # If gear is run within the Readers group, error and exit
         if analysis.parents["group"] == reader_group_id:
